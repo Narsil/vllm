c0e552ba6204e603ad7b47efbbf2245c0de44fbb
Mohit Sharma <mohit21sharma.ms@gmail.com>
2024-07-04 09:55:05 +0000

    kv_head_mapping

--- csrc/ops.h
+++ csrc/ops.h
@@ -5,7 +5,7 @@
 
 void paged_attention_v1(
     torch::Tensor& out, torch::Tensor& query, torch::Tensor& key_cache,
-    torch::Tensor& value_cache, int64_t num_kv_heads, double scale,
+    torch::Tensor& value_cache, torch::Tensor& head_mapping, double scale,
     torch::Tensor& block_tables, torch::Tensor& seq_lens, int64_t block_size,
     int64_t max_seq_len, const c10::optional<torch::Tensor>& alibi_slopes,
     const std::string& kv_cache_dtype, double k_scale, double v_scale,
@@ -16,7 +16,7 @@ void paged_attention_v1(
 void paged_attention_v2(
     torch::Tensor& out, torch::Tensor& exp_sums, torch::Tensor& max_logits,
     torch::Tensor& tmp_out, torch::Tensor& query, torch::Tensor& key_cache,
-    torch::Tensor& value_cache, int64_t num_kv_heads, double scale,
+    torch::Tensor& value_cache, torch::Tensor& head_mapping, double scale,
     torch::Tensor& block_tables, torch::Tensor& seq_lens, int64_t block_size,
     int64_t max_seq_len, const c10::optional<torch::Tensor>& alibi_slopes,
     const std::string& kv_cache_dtype, double k_scale, double v_scale,
